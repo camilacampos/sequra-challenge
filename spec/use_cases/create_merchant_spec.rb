62afd9c4fb148c2945b7b3199c403f60cf8a9b48
Camila Campos <camila@genialcare.com.br>
2024-02-29 00:40:53 -0300

    move create merchant use case

--- spec/use_cases/create_merchant_spec.rb
+++ spec/use_cases/create_merchant_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe ::Merchants::Create do
+RSpec.describe ::CreateMerchant do
   context "when any attribute is invalid" do
     it "raises an error" do
       expect {
