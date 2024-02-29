31c8d6b813c484c832c7e0fd3b0866b9338e5419
Camila Campos <camila@genialcare.com.br>
2024-02-29 19:55:09 -0300

    improve calculate fee use case

--- app/use_cases/calculate_fee.rb
+++ app/use_cases/calculate_fee.rb
@@ -1,11 +1,12 @@
 class CalculateFee
   def call(order)
+    percentage = 0.85 / 100.0 # 0.85%
     if order.amount < Money.from_amount(50)
-      1 / 100.0 # 1%
+      percentage = 1 / 100.0 # 1%
     elsif order.amount < Money.from_amount(300)
-      0.95 / 100.0 # 0.95%
-    else
-      0.85 / 100.0 # 0.85%
+      percentage = 0.95 / 100 # 0.95%
     end
+
+    Fee.new(percentage:, from_amount: order.amount)
   end
 end
--- spec/use_cases/calculate_fee_spec.rb
+++ spec/use_cases/calculate_fee_spec.rb
@@ -4,22 +4,55 @@ RSpec.describe ::CalculateFee do
   it "returns 1% when amount is less than 50" do
     order = build(:order, amount: Money.from_amount(49))
 
-    expect(described_class.new.call(order)).to eq 1 / 100.0
+    fee = described_class.new.call(order)
+
+    expect(fee.percentage).to eq 1 / 100.0
+    expect(fee.order_amount).to eq order.amount
+    expect(fee.value).to eq(order.amount * 1 / 100.0)
+    expect(fee.disbursed_amount).to eq(order.amount - fee.value)
+  end
+
+  it "returns 0.95% when amount is equal to 50" do
+    order = build(:order, amount: Money.from_amount(50))
+
+    fee = described_class.new.call(order)
+
+    expect(fee.percentage).to eq 0.95 / 100.0
+    expect(fee.order_amount).to eq order.amount
+    expect(fee.value).to eq(order.amount * 0.95 / 100.0)
+    expect(fee.disbursed_amount).to eq(order.amount - fee.value)
   end
 
   it "returns 0.95% when amount is between 50 and 300" do
-    order1 = build(:order, amount: Money.from_amount(50))
-    order2 = build(:order, amount: Money.from_amount(299))
+    order = build(:order, amount: Money.from_amount(299))
 
-    expect(described_class.new.call(order1)).to eq 0.95 / 100.0
-    expect(described_class.new.call(order2)).to eq 0.95 / 100.0
+    fee = described_class.new.call(order)
+
+    expect(fee.percentage).to eq 0.95 / 100.0
+    expect(fee.order_amount).to eq order.amount
+    expect(fee.value).to eq(order.amount * 0.95 / 100.0)
+    expect(fee.disbursed_amount).to eq(order.amount - fee.value)
   end
 
-  it "returns 0.85% when amount is greater than or equal to 300" do
-    order1 = build(:order, amount: Money.from_amount(300))
-    order2 = build(:order, amount: Money.from_amount(500))
+  it "returns 0.85% when amount is equal to 300" do
+    order = build(:order, amount: Money.from_amount(300))
 
-    expect(described_class.new.call(order1)).to eq 0.85 / 100.0
-    expect(described_class.new.call(order2)).to eq 0.85 / 100.0
+    fee = described_class.new.call(order)
+
+    expect(fee.percentage).to eq 0.85 / 100.0
+    expect(fee.order_amount).to eq order.amount
+    expect(fee.value).to eq(order.amount * 0.85 / 100.0)
+    expect(fee.disbursed_amount).to eq(order.amount - fee.value)
+  end
+
+  it "returns 0.85% when amount is greater than 300" do
+    order = build(:order, amount: Money.from_amount(301))
+
+    fee = described_class.new.call(order)
+
+    expect(fee.percentage).to eq 0.85 / 100.0
+    expect(fee.order_amount).to eq order.amount
+    expect(fee.value).to eq(order.amount * 0.85 / 100.0)
+    expect(fee.disbursed_amount).to eq(order.amount - fee.value)
   end
 end
